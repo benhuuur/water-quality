0e6eee8605e14839fd1f67287fc7ce204fa16344
benhur <benhurfeld@gmail.com>
2025-01-07 23:52:22 -0300

    Decision tree classifier

---
+++ tree.py
@@ -0,0 +1,20 @@
+import pandas as pd
+from sklearn.metrics import classification_report
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier, plot_tree
+import matplotlib.pyplot as plt
+
+df = pd.read_csv(r"data\water_potability_imputed.csv")
+
+X = df.drop("Potability", axis=1)
+y = df["Potability"]
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.5, random_state=0)
+
+model = DecisionTreeClassifier(random_state=0)
+model.fit(X_train, y_train)
+
+y_pred = model.predict(X_test)
+
+print(classification_report(y_test, y_pred))
